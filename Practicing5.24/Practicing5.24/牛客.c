51d6bfe9e553062361463711b591e82e625d56d7
AZhangxiansheng <2677504704@qq.com>
2021-05-24 21:56:00 +0800

    练习

---
+++ Practicing5.24/Practicing5.24/牛客.c
@@ -0,0 +1,98 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+//{
+//	long a;
+//	scanf("%ld", &a);
+//	printf("%ld", a * 31560000);
+//	return 0;
+//}
+
+//int main()
+//{
+//	float a;
+//	scanf("%f", &a);
+//	printf("%d", (int)a % 10);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a, b;
+//	scanf("%d %d", &a, &b);
+//	printf("%d", (a + b) % 100);
+//	return 0;
+//}
+
+
+//int main(){
+//	int a = 40, c = 212;
+//	printf("%d\n", (-8 + 22)*a - 10 + c / 2);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char c;
+//	scanf("%c", &c);
+//	printf("%d", c);
+//}
+
+//int main()
+//{
+//	int a, b;
+//	scanf("a=%d,b=%d", &a, &b);
+//	printf("a=%d,b=%d", b, a);
+//	return 0;
+//}
+
+//int main(){
+//	int year;
+//	int month;
+//	int date;
+//	scanf("%4d%2d%2d", &year, &month, &date);
+//	printf("year=%d\n", year);
+//	printf("month=%02d\n", month);
+//	printf("date=%02d", date);
+//}
+
+//int main()
+//{
+//	int arr[] = { 73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116, 33 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (int i = 0; i < sz; i++)
+//	{
+//		printf("%c",arr[i]);
+//	}
+//	return 0;
+//}
+
+
+//int main(void)
+//{
+//	char a;
+//	scanf("%c", &a);
+//	int i = 0, j = 0, k = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		for (j = 4; j>i; j--)
+//		{
+//			printf(" ");
+//		}
+//		for (k = 0; k <= i; k++)
+//		{
+//			printf("%c ",a);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//
+//	printf("\n%d\n", printf("Hello world!"));
+//	return 0;
+//}
