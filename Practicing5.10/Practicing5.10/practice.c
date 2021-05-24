6cced42e669393b0ec4dcd3592838b712c6557b4
AZhangxiansheng <2677504704@qq.com>
2021-05-24 21:58:51 +0800

    练习

---
+++ Practicing5.10/Practicing5.10/practice.c
@@ -0,0 +1,74 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<stdio.h>
+
+
+//int main(void)
+//{
+//	printf("Practice makes perfect!");
+//	return 0;
+//}
+
+//int main(void)
+//{
+//	printf("v   v\n");
+//	printf(" v v\n");
+//	printf("  v\n");
+//	return 0;
+//}
+
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	printf("The size of short is %d bytes.\n", sizeof(short));
+//	printf("The size of int is %d bytes.\n", sizeof(int));
+//	printf("The size of long is %d bytes.\n", sizeof(long));
+//	printf("The size of long long is %d bytes.", sizeof(long long));
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main(void)
+//{
+//	printf("     **     \n");
+//	printf("     **     \n");
+//	printf("************\n");
+//	printf("************\n");
+//	printf("    *  *    \n");
+//	printf("    *  *    \n");
+//}
+
+
+
+//#include <stdio.h>
+//int main(void)
+//{
+//	printf("0%o 0X%X", 1234, 1234);
+//	return 0;
+//}
+
+
+
+//#include <stdio.h>
+//int main(void){
+//	printf("%15d", 0xABCDEF);
+//	return 0;
+//}
+
+
+
+//#include<stdio.h>
+//int main(void)
+//{
+//	int a, b, c;
+//	scanf("%d %d %d", &a, &b, &c);
+//	printf("score1=%d,score2=%d,score3=%d\n", a, b, c);
+//	return 0;
+//}
+
+
+
+
+
