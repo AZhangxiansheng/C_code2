884bc2f1ad8533c68d6ce6cd4812fe99d22bb36f
AZhangxiansheng <2677504704@qq.com>
2021-05-30 21:36:28 +0800

    练习

---
+++ Practicing5.30/Practicing5.30/牛客.c
@@ -0,0 +1,128 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+
+
+
+//int main()
+//{
+//	int a, b, c, d, e;
+//	scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
+//	printf("%.1f\n", (a + b + c + d + e) / 5.0);
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	printf("Name    Age    Gender\n");
+//	printf("---------------------\n");
+//	printf("Jack    18     man\n");
+//}
+
+
+
+//int main()
+//{
+//	printf("I lost my cellphone!");
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int n, h, m,y;
+//	scanf("%d %d %d", &n, &h, &m);
+//	y = m / h;
+//	if (m%h != 0){
+//		y += 1;
+//	}
+//	printf("%d", n - y);
+//	return 0;
+//}
+
+
+
+
+//#include<math.h>
+//int main()
+//{
+//	int n = 0;
+//	while (scanf("%d", &n) != EOF){
+//		printf("%.0f\n", pow(2, n));
+//		getchar();
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	char c;
+//	while (scanf("%c", &c) != EOF){
+//		printf("%c\n",c+32);
+//		getchar();
+//	}
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	double r, v;
+//	scanf("%lf", &r);
+//	v = 4 * 3.1415926*r*r*r / 3;
+//	printf("%.3lf", v);
+//	return 0;
+//}
+
+
+
+
+//#include<math.h>
+//int main()
+//{
+//	float a, b, c, p;
+//
+//	scanf("%f%f%f", &a, &b, &c);
+//	p = (a + b + c) / 2;
+//	printf("circumference=%.2f area=%.2f\n", a + b + c, sqrt(p*(p - a)*(p - b)*(p - c)));
+//
+//}
+
+
+
+//int main()
+//{
+//	float w, h;
+//	scanf("%f %f", &w, &h);
+//	h = h / 100;
+//	printf("%.2f", w / (h*h));
+//	return 0;
+//}
+
+//int main()
+//{
+//	
+//	float sum, aver, a, b, c;
+//	scanf("%f %f %f", &a, &b, &c);
+//	sum = a + b + c;
+//	aver = sum*1.0 / 3;
+//	printf("%.2f %.2f", sum, aver);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int hou, min, sec;
+//	scanf("%d", &sec);
+//	hou = sec % 3600;
+//	min = sec % 3600 / 60;
+//	sec = sec / 60;
+//	printf("%d %d %d", hou, min, sec);
+//	return 0;
+//}
